ad5e91d9519ba6ca70d8a8e0fcf82c913c660b80
SDarbyson <Sean.Darbyson@johnabbottcollege.net>
2024-12-12 14:04:29 -0500

    some more proof of concept work, will change to calendar

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@ using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using NextStop.Data;
 using NextStop.Models;
 
@@ -20,23 +21,51 @@ public class IndexModel : PageModel
         _logger = logger;
     }
 
-    [BindProperty, Required, Display(Name = "Origin")]
-    [StringLength(40, MinimumLength = 1, ErrorMessage = "Please enter an origin that does not exceed 40 characters.")]
+    public bool SearchWasRequested {get; private set; }
+
+    [BindProperty, Display(Name = "Origin")]
     public string NewOrigin {get; set; }
 
-    [BindProperty, Required, Display(Name = "Destination")]
-    [StringLength(40, MinimumLength = 1, ErrorMessage = "Please enter a destination that does not exceed 40 characters.")]
+    [BindProperty, Display(Name = "Destination")]
     public string NewDestination {get; set; }
 
-    [BindProperty, Required, Display(Name = "Weekday of Trip")]
+    [BindProperty, Display(Name = "Weekday of Trip")]
     public DayOfWeek? NewDepartureDay {get; set; }
 
-    public async Task<IActionResult> OnGetAsync()
+    public IList<Trip> Trips {get; set; } = new List<Trip>();
+
+    public async Task<IActionResult> OnGetAsync(string? origin,
+                                                string? destination,
+                                                DayOfWeek? day)
     {
         var curUser = await _userManager.GetUserAsync(User);
         if (curUser == null) {
                 return RedirectToPage("/Account/Login", new {area = "Identity" });
             }
+
+        SearchWasRequested = Request.QueryString.Value?.Length > 1;
+
+        if (SearchWasRequested)
+        {
+            var query = context.Trips.AsQueryable();
+
+            if (!string.IsNullOrEmpty(origin))
+            {
+                query = query.Where(t => t.Origin.Contains(origin));
+            }
+
+            if (!string.IsNullOrEmpty(destination))
+            {
+                query = query.Where(t => t.Destination.Contains(destination));
+            }
+
+            if (day.HasValue)
+            {
+                query = query.Where(t => t.DepartureDay == day.Value);
+            }
+
+            Trips = await query.ToListAsync();
+        }
 
         return Page();
     }
