f227cc1658cc335bced4bfcbf94bbe6262d781c1
SDarbyson <Sean.Darbyson@johnabbottcollege.net>
2024-12-14 11:58:53 -0500

    TripInfo page that can be used to connect with Orders

--- Pages/TripInfo.cshtml.cs
+++ Pages/TripInfo.cshtml.cs
@@ -24,6 +24,7 @@ namespace NextStop.Pages
         public Trip Trip;
         public DateTime DateOfTravel { get; set; }
         public int NumberOfPassengers { get; set; }
+        public decimal Total { get; set; }
         public bool OrderButtonActive { get; set; } = false;
 
         public async Task<IActionResult> OnGetAsync(int? tripId, DateTime? date, int? numPassengers)
@@ -32,33 +33,43 @@ namespace NextStop.Pages
             if (curUser == null) {
                 return RedirectToPage("/Account/Login", new {area = "Identity" });
             }
-            if (!tripId.HasValue || !date.HasValue || !numPassengers.HasValue) {
-                RedirectToPage("/Index");
-                // DOESNT WORK PROPERLY
-                // TODO Error message
+            if (tripId.HasValue && date.HasValue && numPassengers.HasValue) {
+                
+                Trip = await context.Trips
+                    .Include(t => t.Bus)
+                    .FirstOrDefaultAsync(t => t.Id == tripId);
+                
+                if (Trip == null)
+                {
+                    TempData["ErrorMessage"] = "No trip was found.";
+                    _logger.LogError("TripInfo page Trip ID search returned on valid trip");
+                    return RedirectToPage("/Index");
+                }
+
+                DateTime day = (DateTime) date;
+                day = day.Date;
+                DateOfTravel = day;
+
+                int capacity = Trip.Bus.Capacity;
+
+                int numCurrentPassengers = context.Orders
+                    .Where(o => o.Trip.Id == tripId && o.DateOfTravel.Date == day)
+                    .Select(o => o.NumOfPassengers)
+                    .DefaultIfEmpty()
+                    .Sum();
+
+                if ((capacity - numCurrentPassengers) > numPassengers)
+                {
+                    OrderButtonActive = true;
+                }
+                NumberOfPassengers = (int)numPassengers;
+                Total = Trip.Price * NumberOfPassengers;
+                
+                return Page();
             }
-            Trip = await context.Trips
-                .Include(t => t.Bus)
-                .FirstOrDefaultAsync(t => t.Id == tripId);
-
-            DateTime day = (DateTime) date;
-            day = day.Date;
-            DateOfTravel = day;
-
-            int capacity = Trip.Bus.Capacity;
-
-            int numCurrentPassengers = context.Orders
-                .Where(o => o.Trip.Id == tripId && o.DateOfTravel.Date == day)
-                .Select(o => o.NumOfPassengers)
-                .DefaultIfEmpty()
-                .Sum();
-
-            if ((capacity - numCurrentPassengers) > numPassengers)
-            {
-                OrderButtonActive = true;
-            }
-            
-            return Page();
+            TempData["ErrorMessage"] = "An error occurred when loading the page";
+            _logger.LogError("TripInfo page 1 or more parameters was missing a value");
+            return RedirectToPage("/Index");
         }
     }
 }
